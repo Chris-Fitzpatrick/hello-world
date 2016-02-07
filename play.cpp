3a993ab66eea10f61389a96cae20065dfce2a17b
Chris-Fitzpatrick <chrishfitzpatrick@gmail.com>
2016-02-07 16:30:55 -0500

    Second commit

---
+++ play.cpp
@@ -0,0 +1,25 @@
+//
+//  div.cpp
+//  
+//
+//  Created by Chris Fitzpatrick on 7/16/15.
+//
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+int main(){
+        
+        string one= "TESTING";
+        string two= "TESTING";
+        if (one == two)
+        cout << "they are the same" << endl;
+    
+    
+    return 0;
+}
+
